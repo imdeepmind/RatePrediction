1d70cfae7f8557698908161fb6f05d71dbaff954
imdeepmind <infinityatme@gmail.com>
2019-03-10 00:08:00 +0530

    added preprocessing of text data

--- model.py
+++ model.py
@@ -1,41 +1,38 @@
 import numpy as np
 import pandas as pd
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-import re
+from keras.preprocessing.text import one_hot
+from keras.preprocessing.sequence import pad_sequences
 
+VOCAB_SIZE = 50000
+MAX_LENGTH = 50
+NO_OF_CLASSES = 5
 meta = pd.read_csv('dataset/meta.csv')
 
 index = meta['id']
-star = meta['star']
+stars = meta['star']
 
 del meta
 
-def clean_review(review):
-    review = review.lower()
-
-    review = re.sub(r'''(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))''', '', review, flags=re.MULTILINE)
-    review = re.sub(r'(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)', '', review)
-    review = re.sub(r'\b[0-9]+\b\s*', '', review)
-
-    review = word_tokenize(review)
-
-    review = [t for t in review if t.isalpha()]
-
-    stop_words = set(stopwords.words('english'))
-    review = [t for t in review if not t in stop_words]
-
-    return review
-
 def generate_batch(index, stars, batch_size):
     counter = 0
+    
     while True:
         tempIndex = index[counter:counter + batch_size]
         reviews = []
-        for re in tempIndex:
-            with open('dataset/reviews/' + str(re) + '.txt', 'r') as file:
-                reviews.append(file.read())
+        
+        for rev in tempIndex:
+            with open('dataset/reviews/' + str(rev) + '.txt', 'r') as file:
+                review = file.read()
+
+                review_hot = one_hot(review, VOCAB_SIZE)
+                
+                reviews.append(review_hot)
+
+        reviews = pad_sequences(reviews, maxlen=MAX_LENGTH, padding='post')
+                
         tempStars = stars[counter:counter + batch_size]
+        
         counter = (counter + batch_size) % len(index)
+        
         yield reviews, tempStars
 
