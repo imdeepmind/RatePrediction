6f81cae6611dba5ee607f16974627b118ea0f07e
imdeepmind <infinityatme@gmail.com>
2019-03-09 23:22:11 +0530

    added a method to clean reviews

--- model.py
+++ model.py
@@ -1,5 +1,8 @@
 import numpy as np
 import pandas as pd
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import re
 
 meta = pd.read_csv('dataset/meta.csv')
 
@@ -7,6 +10,22 @@ index = meta['id']
 star = meta['star']
 
 del meta
+
+def clean_review(review):
+    review = review.lower()
+
+    review = re.sub(r'''(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))''', '', review, flags=re.MULTILINE)
+    review = re.sub(r'(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)', '', review)
+    review = re.sub(r'\b[0-9]+\b\s*', '', review)
+
+    review = word_tokenize(review)
+
+    review = [t for t in review if t.isalpha()]
+
+    stop_words = set(stopwords.words('english'))
+    review = [t for t in review if not t in stop_words]
+
+    return review
 
 def generate_batch(index, stars, batch_size):
     counter = 0
@@ -19,3 +38,4 @@ def generate_batch(index, stars, batch_size):
         tempStars = stars[counter:counter + batch_size]
         counter = (counter + batch_size) % len(index)
         yield reviews, tempStars
+
