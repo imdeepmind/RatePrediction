75c03fa7b965b3375bc6b2dd6418baa8b833495e
imdeepmind <infinityatme@gmail.com>
2019-03-10 19:41:03 +0530

    added validation system in the dumb model

--- model.py
+++ model.py
@@ -5,6 +5,7 @@ from keras.preprocessing.sequence import pad_sequences
 from keras.models import Sequential
 from keras.layers import Dense, Flatten
 from keras.layers.embeddings import Embedding
+from sklearn.model_selection import train_test_split
 
 VOCAB_SIZE = 50000
 MAX_LENGTH = 50
@@ -14,13 +15,15 @@ meta = pd.read_csv('dataset/meta.csv')
 index = meta['id'].values
 stars = pd.get_dummies(meta['star']).values
 
+X_train, X_test, y_train, y_test = train_test_split(index, stars, test_size=0.1, random_state=1969)
+
 del meta
 
-def generate_batch(index, stars, batch_size):
+def generate_batch(X, y, batch_size):
     counter = 0
     
     while True:
-        tempIndex = index[counter:counter + batch_size]
+        tempIndex = X[counter:counter + batch_size]
         reviews = []
         
         for rev in tempIndex:
@@ -33,19 +36,18 @@ def generate_batch(index, stars, batch_size):
 
         reviews = pad_sequences(reviews, maxlen=MAX_LENGTH, padding='post')
                 
-        tempStars = stars[counter:counter + batch_size]
+        tempStars = y[counter:counter + batch_size]
         
         counter = (counter + batch_size) % len(index)
         
         yield reviews, tempStars
 
-
-
-
 model = Sequential()
 
 model.add(Embedding(VOCAB_SIZE, 32, input_length=MAX_LENGTH))
+
 model.add(Flatten())
+
 model.add(Dense(250, activation='relu'))
 model.add(Dense(NO_OF_CLASSES, activation='softmax'))
 
@@ -53,8 +55,11 @@ model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy']
 
 print(model.summary())
 
-gen = generate_batch(index=index, stars=stars, batch_size=32)
-model.fit_generator(gen,
-                    epochs=10,
-                    steps_per_epoch=100)
+train_gen = generate_batch(X=X_train, y=y_train, batch_size=32)
+test_gen = generate_batch(X=X_test, y=y_test, batch_size=32)
 
+model.fit_generator(train_gen,
+                    epochs=10,
+                    steps_per_epoch=100,
+                    validation_data=test_gen,
+                    validation_steps=50)
