45d6833419ac6388b47856dd4a3aa9a56d8eb62d
imdeepmind <infinityatme@gmail.com>
2019-03-13 00:17:16 +0530

    updated the predict system

--- predict.py
+++ predict.py
@@ -1,11 +1,32 @@
 import keras
 from keras.preprocessing.text import one_hot
 from keras.preprocessing.sequence import pad_sequences
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import re
 
 VOCAB_SIZE = 50000
 MAX_LENGTH = 80
 
+def clean_review(review):
+    review = review.lower()
+    review = re.sub(r'''(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))''', '', review, flags=re.MULTILINE)
+    review = re.sub(r'(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)', '', review)
+    review = re.sub(r'\b[0-9]+\b\s*', '', review)
+    return review
+
+
 def process_review(review):
+    review = clean_review(review)
+                
+    review = word_tokenize(review)
+    
+    review = [t for t in review if t.isalpha()]
+    stop_words = set(stopwords.words('english'))
+    review = [t for t in review if not t in stop_words]
+    
+    review = " ".join(review)
+    
     review = one_hot(review, VOCAB_SIZE)
     
     review = pad_sequences([review], maxlen=MAX_LENGTH, padding='post')
