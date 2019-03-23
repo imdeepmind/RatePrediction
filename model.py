9f6ecf89567de9c2fc80211d520d1e2647655527
imdeepmind <infinityatme@gmail.com>
2019-03-23 23:07:15 +0530

    updated the model

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
-from keras.layers import LSTM, Dense
+from keras.layers import CuDNNLSTM, Dense, Dropout, GlobalMaxPool1D, Bidirectional
 from keras.layers.embeddings import Embedding
 
 with open(r"data/word_tokenizer.pickle", "rb") as input_file:
@@ -30,13 +30,16 @@ del X, y
 
 model = Sequential()
 model.add(Embedding(VOCAB_SIZE, 32, input_length=MAX_WORDS))
-model.add(LSTM(32, activation='relu'))
+model.add(Bidirectional(CuDNNLSTM(64, return_sequences = True)))
+model.add(GlobalMaxPool1D())
+model.add(Dense(40, activation="relu"))
+model.add(Dropout(0.05))
 model.add(Dense(NO_OF_CLASSES, activation='softmax'))
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 print(model.summary())
 
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=5, batch_size=64)
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=3, batch_size=128)
 
 model.save('data/model.h5')
