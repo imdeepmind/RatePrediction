36a9c78dda2176e237604b17efd65ac7cd7a0c36
imdeepmind <infinityatme@gmail.com>
2019-03-25 16:42:08 +0530

    added deploy code

---
+++ demo-app/webapp/build/precache-manifest.a8ae5820e6f17f01ba989f26c1b49811.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a10e50cd979e69ad259c",
+    "url": "/static/js/main.9608f032.chunk.js"
+  },
+  {
+    "revision": "2952e7f6140c0321f034",
+    "url": "/static/js/2.9833bcae.chunk.js"
+  },
+  {
+    "revision": "2952e7f6140c0321f034",
+    "url": "/static/css/2.e4bd94bf.chunk.css"
+  },
+  {
+    "revision": "7e2f18d72d909e301733a109dacca667",
+    "url": "/index.html"
+  }
+];
