b5730c18fc4314d20b4358efdd28bffb9cfa0ae9
imdeepmind <infinityatme@gmail.com>
2019-03-25 16:51:19 +0530

    added homepage in webapp

---
+++ demo-app/webapp/build/precache-manifest.fb3645720b726cbf9afea14ecc9b575d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "63e6e5efa6a282144ab9",
+    "url": "/rate-prediction-using-amazon-product-review-dataset/static/js/runtime~main.19c2044d.js"
+  },
+  {
+    "revision": "6bdc154fd5d4c611e8d2",
+    "url": "/rate-prediction-using-amazon-product-review-dataset/static/js/main.117f9e6d.chunk.js"
+  },
+  {
+    "revision": "2952e7f6140c0321f034",
+    "url": "/rate-prediction-using-amazon-product-review-dataset/static/js/2.9833bcae.chunk.js"
+  },
+  {
+    "revision": "2952e7f6140c0321f034",
+    "url": "/rate-prediction-using-amazon-product-review-dataset/static/css/2.e4bd94bf.chunk.css"
+  },
+  {
+    "revision": "ca044e8a1d8640d5efa4390f09bf6c9c",
+    "url": "/rate-prediction-using-amazon-product-review-dataset/index.html"
+  }
+];
--- demo-app/webapp/build/service-worker.js
+++ demo-app/webapp/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a8ae5820e6f17f01ba989f26c1b49811.js"
+  "/rate-prediction-using-amazon-product-review-dataset/precache-manifest.fb3645720b726cbf9afea14ecc9b575d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/rate-prediction-using-amazon-product-review-dataset/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
